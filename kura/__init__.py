9c492986d56cd88fbb7aaa3ab6171ac22f3bb677
Ivan Leo <ivanleomk@gmail.com>
2025-05-30 21:12:04 +0800

    refactor: Remove unused import of summarise_conversations from __init__.py

--- kura/__init__.py
+++ kura/__init__.py
@@ -1,15 +1,13 @@
 from .v1.kura import (
-    summarise_conversations,
     generate_base_clusters_from_conversation_summaries,
     reduce_clusters_from_base_clusters,
     reduce_dimensionality_from_clusters,
-    CheckpointManager,
 )
 from .cluster import ClusterModel
 from .meta_cluster import MetaClusterModel
 from .summarisation import SummaryModel, summarise_conversations
-from .types import Conversation
 from .checkpoint import CheckpointManager
+from .types import Conversation
 
 __all__ = [
     "ClusterModel",
